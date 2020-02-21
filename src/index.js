bab2f68e908d1491227b5dce5c62bbfaa545089d
Yan Andryshkin <yan.7feb@gmail.com>
2020-02-21 19:24:11 +0300

    feat: solve towel-sort

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort (arr) {
+  if (!arr || !arr.length) return new Array(0)
+  return arr.map((el, pos) => pos % 2 != 0 ? el.sort((a,b) => b - a) : el)
+            .map(el => el.join(','))
+            .join(',')
+            .split(',')
+            .map(el => parseInt(el));
 }
